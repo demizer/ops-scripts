a6765fc65466c3186d6645ecc00a68fafd92b7ef
Jesus Alvarez <jeezusjr@gmail.com>
2025-11-15 15:33:40 -0800

    dotfiles: fix ll alias with eza

--- dotfiles/fish-config-linux.fish
+++ dotfiles/fish-config-linux.fish
@@ -1,5 +1,5 @@
-if type -q exa
-    alias ll "exa -l -g --icons --group-directories-first"
+if type -q eza
+    alias ll "eza -l -g --icons --group-directories-first"
     alias lla "ll -a"
     alias nvimdiff "nvim -d"
     alias vimdiff "nvimdiff"
